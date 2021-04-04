e33d1ac6c75aecbc84b311ffd66e9748f68580cd
kkvoid <79696314+kkvoid@users.noreply.github.com>
2021-04-04 21:39:58 +0800

    Update Generate.cpp

--- Generate.cpp
+++ Generate.cpp
@@ -20,6 +20,8 @@ int rd() // generate a random number
 
 int chki(int a[11][11], int len, int B_size, int st1, int st2)//check i lines to place ship 
 {
+	//len == length of the ship    B_size  ==  Board Size
+	//(st1,st2) is the starter position to check is there is a place for the ship
 	int p;
 	for(int i = 0; i<B_size; ++i)
 	{
@@ -59,6 +61,7 @@ int chkj(int a[11][11], int len, int B_size, int st1, int st2)//check j lines to
 }
 int PLACE_SHIP(int a[11][11], int n, int len)
 {
+	// n*n == Board Size  & len == ship lenth
 	int r1,r2,r3,siz,ci,cj;
 	r1 = rd();
 	r2 = rd();
@@ -66,20 +69,21 @@ int PLACE_SHIP(int a[11][11], int n, int len)
 	int ii,jj;
 	ii = r1 % (n-len);
 	jj = r2 % (n-len);
-//ii = 0; jj = 0; r3 = 1;
+	//  ii,jj  is the starter position to check
+	//  r3  is the direction ( r3 = 0 means row  &  r3 = 1 means colum )
 	ci = chki(a,len,n,ii,jj);
 	cj = chkj(a,len,n,ii,jj);
-//	printf("ii%d jj%d ci%d cj%d   r3=%d (1->shu  0->heng)\n",ii,jj,ci,cj,r3);
+	
 	if(ci<0 && cj<0) return 0; // no available place
-	// len == ship lenth
-	if(r3) // ship on an i line
+
+	if(r3) // ship on an i line (row)
 	{
 		if(ci >= 0)
 			for(int k = 0; k<len; ++k)	a[ci/100 + k][ci%100] = len;
 		else if(cj >= 0)
 			for(int k = 0; k<len; ++k)	a[cj/100][cj%100 + k] = len;
 	}
-	else // ship on a j line
+	else // ship on a j line (colum)
 	{
 		if(cj >= 0)
 			for(int k = 0; k<len; ++k)	a[cj/100][cj%100 + k] = len;
@@ -92,10 +96,10 @@ int GEN(int a[11][11], int n, int S_num[])//ship number
 {
 	//n * n == board size
 	srand( (time(NULL)%19260817) );
+	
 	int tot = 0;
 	for(int i = 0; i<5; ++i)
 		for(int j = 1; j<=S_num[i]; ++j) tot+= S_num[i]*j;
-	
 	if( tot > (n*n/2) ) return 1; // too many ships
 	if( tot == 0 ) return 2; // no ships
 	
@@ -103,7 +107,7 @@ int GEN(int a[11][11], int n, int S_num[])//ship number
 		for(int j = 1; j<=S_num[i]; ++j)
 			if(PLACE_SHIP(a,n,i+2) == 0) //i+2 == length of ship
 				printf("FAILED TO PLACE %d\n",i+2);
-					
+	
 	return 0;
 }
 
@@ -117,6 +121,7 @@ void PRINT_BOARD(int a[11][11],int n)
 	for(int i = 0; i<n; ++i)
 	{
 		printf("I%d  ",i);
+//0 == nothing   2 == Submarine  3 == Destoryer  4 == Cruiser  5 == Battleship  6 == Carrier
 		for(int j = 0; j<n; ++j)
 		{
 			if(B[i][j])
