c2abcb8ad55cef82bbdba67578f039ca0039b729
kkvoid <79696314+kkvoid@users.noreply.github.com>
2021-04-11 19:59:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,44 +5,38 @@
 using namespace std;
 
 int main(){
-  char opt;
-  cout << "Welcome to the Seabattle game (Author: Tu Yuanyang & He Jiachuan)\n" << endl;
-  cout << "1: Play!\n2: Instruction" << endl;
-  cin >> opt;
+	char opt;
+	cout << "Welcome to the Seabattle game (Author: Tu Yuanyang & He Jiachuan)\n\n";
+	cout << "    1: Play!\n    2: Instruction" << endl;
+	cin >> opt;
+	system("cls");
+	
+	while(opt != '1')
+	{
+		if(opt != '2')
+			cout << "Please input again\n\n    1: Play!\n    2: Instruction"<< endl;
+		else if(opt == '2')
+		{
+			instruction();
+			cout << "    1: Play!\n    2: Instruction" << endl;
+		}
+		cin>>opt;
+		system("cls");
+	}
+	
+	cout << "Choosing Mode:\n" << endl;
+	cout << "    1: Fight Against AI" << endl;
+	cout << "    2: Challenge Mode" << endl;
+	cin >> opt;
+	system("cls");
+	
+	if (opt == '2'){
+		challenge();
+		cout << "    Gameover";
+	}
+	else if (opt == '1'){
+	
+	}
 
-  while (opt != '1' && opt != '2'){
-    cout << "Please input again" << endl;
-    cin >> opt;
-  }
-
-  system("cls");
-
-  if (opt == '2'){
-    instruction();
-    cin >> opt;
-  }
-
-  system("cls");
-
-  cout << "Choosing Mode:\n" << endl;
-  cout << "1: Fight Against AI" << endl;
-  cout << "2: Challenge Mode" << endl;
-  cin >> opt;
-
-  while (opt != '1' && opt != '2'){
-    cout << "Please input again" << endl;
-    cin >> opt;
-  }
-
-  system("cls");
-
-  if (opt == '2'){
-    challenge();
-    cout << "Gameover";
-  }
-  else if (opt == '1'){
-
-  }
-
-
+	return 0;
 }
