40e32946169ff14b5c509a26f749736533d57df5
He Jiangchuan <79696314+kkvoid@users.noreply.github.com>
2021-04-24 17:31:35 +0800

    Create input_ships_shell.h

---
+++ input_ships_shell.h
@@ -0,0 +1,8 @@
+#ifndef INPUT_SHIPS_SHELL_H
+#define INPUT_SHIPS_SHELL_H
+
+void print_current_ships(int ships[]);
+int input_ships_shell(int ships[], int &shell);
+int input_ships(int ships[]);
+
+#endif
