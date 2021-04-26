109ac4498dfd877d6326da58d71e66b4bb9bfabf
He Jiangchuan <79696314+kkvoid@users.noreply.github.com>
2021-04-26 18:24:36 +0800

    Update AI.h

--- AI.h
+++ AI.h
@@ -1,13 +1,6 @@
-// The head file for AI.cpp
-
 #ifndef AI_H
 #define AI_H
 
-int IMPOSSIBLE(int a[11][11], int n);
-int EASY(int a[11][11], int n);
-int HARD(int a[11][11], int n);
-int NORMAL(int a[11][11], int n);
-int Count_ships_left_AI(int board[][11], int ships[]);
-int AI(int diff);
+char AI(int diff);
 
 #endif
