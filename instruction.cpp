6e329b0f93fa4e1cfefc0f6512e0535b7691cb06
Ryannn <78694466+RyanYuanyang@users.noreply.github.com>
2021-04-26 18:07:18 +0800

    Update instruction.cpp

--- instruction.cpp
+++ instruction.cpp
@@ -7,24 +7,30 @@ using namespace std;
 
 //this function is to print the instruction of the game
 void instruction(){
-  cout << "Battleship game is a strategy type gussing game. \n" << endl;
+  cout << endl;
+  cout << endl;
+  cout << endl;
+  cout << endl;
 
-  cout << "It is typically played on square and ruled grids on which each player's fleet of battleships are marked. " << endl;
 
-  cout << "The locations of the fleets are concealed from the other player. " << endl;
+  cout << "     Sea battle game is a strategy type gussing game. \n" << endl;
 
-  cout << "Players alternate turns calling attacks at the grid which marks the other player's ships, and the objective of the game is to destroy the opposing player's fleet.\n" << endl;
+  cout << "     It is typically played on square and ruled grids on which each player's fleet of battleships are marked. \n" << endl;
 
-  cout << "This game has 2 modes: " << endl;
+  cout << "     The locations of the fleets are concealed from the other player. \n" << endl;
 
-  cout << "  Mode 1: Play Against AI" << endl;
+  cout << "     Players alternate turns calling attacks at the grid which marks the other player's ships, and the objective of the game is to destroy the opposing player's fleet.\n" << endl;
 
-  cout << "  Mode 2: Challenge Mode\n" << endl;
+  cout << "     This game has 2 modes: " << endl;
 
-  cout << "Detailed machenisms of each mode will be introduced before the game start. \n" << endl;
+  cout << "       Mode 1: Play Against AI" << endl;
 
-  cout << "ENJOY AND GET READY FOR THE WAR CAPITAN!\n" << endl;
+  cout << "       Mode 2: Challenge Mode\n" << endl;
 
-  cout << "Enter anything except 3 to Play, or enter 3 to quit.\n" << endl;
+  cout << "     Detailed machenisms of each mode will be introduced before the game start. \n" << endl;
+
+  cout << "     ENJOY AND GET READY FOR THE WAR CAPITAN!\n" << endl;
+
+  cout << "     Enter anything except 3 to Play, or enter 3 to quit.\n" << endl;
 
 }
